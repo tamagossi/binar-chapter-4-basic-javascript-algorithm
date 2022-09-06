30ab315181b2a138ba8d8b940bbc39e4ec93f0e5
Raka Pratama <tama@sampingan.co.id>
2022-09-06 08:50:39 +0700

    feat(): add bubble sort

--- 2-sorting.js
+++ 2-sorting.js
@@ -33,3 +33,27 @@ console.debug(
 
 const randomNumbers = [1, 45, 2, 34, 6, 90, 11, 59, 22, 3];
 console.debug('Sorting problem --->', randomNumbers.sort());
+
+// --- Sorting algorithm
+
+const arrayToSort = [1, 4, 63, 324, 547, 57, 5, 12, 87, 45, 23];
+
+// --- Bubble sort
+
+const bubbleSort = (array) => {
+	const length = array.length;
+
+	for (i = 0; i < length; i++) {
+		for (j = 0; j < length; j++) {
+			if (array[j] > array[j + 1]) {
+				const temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+
+	return array;
+};
+
+console.debug('Bubble sort --->', bubbleSort(arrayToSort));
